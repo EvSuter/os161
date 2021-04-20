b0ffc802f852e76db355057e9733eb12c19e1da7
EvSuter <72505624+EvSuter@users.noreply.github.com>
2021-04-20 15:25:22 +0200

    Update synch.h

--- kern/include/synch.h
+++ kern/include/synch.h
@@ -76,7 +76,7 @@ struct lock {
         char *lk_name;
         HANGMAN_LOCKABLE(lk_hangman);   /* Deadlock detector hook. */
 	int held;
-	char curthread;
+	curthread holder;
         // add what you need here
         // (don't forget to mark things volatile as needed)
 };
